4120b9c02c273167c5811ed5b0c69adf64b518ea
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-03-13 17:16:40 +0100

    Complete task

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,35 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-import { LogoNerdbord } from "../components/LogoNerdbord";
-
-import styles from "../styles/index.module.css";
+import Navbar from "@/components/navbar/Navbar";
+import Animal from "@/components/sections/Animal";
+import styles from "./index.module.css";
+import { useEffect, useState } from "react";
+import { AnimalModel } from "@/types/types";
 
 export default function Home() {
-  return (
-    <div className={styles.wrapper}>
-      <LogoNerdbord />
-      <h1 className={inter.className}>
-        Recruitment task for Javascript Trainee
-      </h1>
-    </div>
-  );
+    const [animals, setAnimals] = useState<AnimalModel[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch("/api/animals");
+            const data = await res.json();
+            setAnimals(data);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div className={styles.wrapper}>
+            <Navbar />
+            <div className={styles.content}>
+                {animals.map((animal) => (
+                    <Animal
+                        key={animal.id}
+                        id={animal.id}
+                        name={animal.name}
+                        title={animal.title}
+                        avatar={animal.avatar}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+export interface AnimalModel {
+    id: string;
+    name: string;
+    title: string;
+    avatar: string;
+}
